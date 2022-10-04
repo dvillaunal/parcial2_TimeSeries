fa807a10eea701a24bbbf31693461d8b75f5a653
DVILLA <56985754+dvillaunal@users.noreply.github.com>
2022-10-04 16:17:09 -0500

    .

--- parcial2.R
+++ parcial2.R
@@ -40,7 +40,41 @@ t2 <- ts(df2$value,start = c(2020,1), frequency = 365)
 
 t3 <- ts(df3$registro,start = c(2019,1), frequency = 365)
 
+dt3 <- t3 %>% diff()
+
 m2 <- auto.arima(t2)
 
+dm3 <- auto.arima(dt3, max.p = 3)
+
+require(lmtest)
+dwtest(dm3)
+
+require(car)
+
+
+
+par(mfrow=c(1,2))
+acf(dt3)
+pacf(dt3)
+
+plot(dm3)
+plot(m2)
+
+checkresiduals(dm3)
+
+summary(dm3)
+
+res <- residuals(naive(dm3$residuals))
+
+Box.test(dm3$residuals, lag = 10, fitdf = T)
+
+gridExtra::grid.arrange(ggAcf(res),ggAcf(dm3$residuals),ncol=2)
+
+
+polyroot(c(1,-0.9761,0.2952,-0.4563,0.6435,-0.2057)) %>% abs()
+
 jarque.bera.test(m2$residuals)
+
+
+auto.arima(dt3)
 
